5150fb487500d48065af0ccb099eec300f4b3cb1
William Blake Galbreath <Blake.Galbreath@GMail.com>
2020-01-03 06:36:01 -0600

    Update LuckPerms dependency to 5.0

--- src/main/java/net/pl3x/bukkit/chatter/hook/LuckPermsHook.java
+++ src/main/java/net/pl3x/bukkit/chatter/hook/LuckPermsHook.java
@@ -1,39 +1,45 @@
 package net.pl3x.bukkit.chatter.hook;
 
-import me.lucko.luckperms.LuckPerms;
-import me.lucko.luckperms.api.Contexts;
-import me.lucko.luckperms.api.User;
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.context.ImmutableContextSet;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.query.QueryOptions;
 import net.pl3x.bukkit.chatter.configuration.Config;
 import org.bukkit.entity.Player;
 
 public class LuckPermsHook {
+    private static LuckPerms API = LuckPermsProvider.get();
+
     public static String getPrefix(Player player) {
-        User user = LuckPerms.getApi().getUser(player.getUniqueId());
-        Contexts context = LuckPerms.getApi().getContextForUser(user).orElseThrow(() ->
+        User user = API.getUserManager().getUser(player.getUniqueId());
+        ImmutableContextSet context = API.getContextManager().getContext(user).orElseThrow(() ->
                 new IllegalStateException("Could not get LuckPerms context for player " + player));
+        QueryOptions query = QueryOptions.contextual(context);
         String prefix;
         if (Config.INHERIT_PREFIXES_FROM_ALL_GROUPS) {
-            prefix = String.join("", user.getCachedData().getMetaData(context).getPrefixes().values());
+            prefix = String.join("", user.getCachedData().getMetaData(query).getPrefixes().values());
         } else {
-            prefix = user.getCachedData().getMetaData(context).getPrefix();
+            prefix = user.getCachedData().getMetaData(query).getPrefix();
         }
         return prefix == null ? "" : prefix;
     }
 
     public static String getSuffix(Player player) {
-        User user = LuckPerms.getApi().getUser(player.getUniqueId());
-        Contexts context = LuckPerms.getApi().getContextForUser(user).orElseThrow(() ->
+        User user = API.getUserManager().getUser(player.getUniqueId());
+        ImmutableContextSet context = API.getContextManager().getContext(user).orElseThrow(() ->
                 new IllegalStateException("Could not get LuckPerms context for player " + player));
+        QueryOptions query = QueryOptions.contextual(context);
         String suffix;
         if (Config.INHERIT_SUFFIXES_FROM_ALL_GROUPS) {
-            suffix = String.join("", user.getCachedData().getMetaData(context).getSuffixes().values());
+            suffix = String.join("", user.getCachedData().getMetaData(query).getSuffixes().values());
         } else {
-            suffix = user.getCachedData().getMetaData(context).getSuffix();
+            suffix = user.getCachedData().getMetaData(query).getSuffix();
         }
         return suffix == null ? "" : suffix;
     }
 
     public static String getPrimaryGroup(Player player) {
-        return LuckPerms.getApi().getUser(player.getUniqueId()).getPrimaryGroup();
+        return API.getUserManager().getUser(player.getUniqueId()).getPrimaryGroup();
     }
 }
